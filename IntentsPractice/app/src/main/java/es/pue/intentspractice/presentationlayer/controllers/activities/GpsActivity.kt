9bb69fe6f9ee1cad2424e621a13313a44fc8914f
Sergio <sergiorp6@gmail.com>
2019-03-11 20:53:13 +0100

    Open Google Maps when a button is clicked using implicit intent

--- IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/GpsActivity.kt
+++ IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/GpsActivity.kt
@@ -1,13 +1,22 @@
 package es.pue.intentspractice.presentationlayer.controllers.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import es.pue.intentspractice.R
+import kotlinx.android.synthetic.main.activity_gps.*
 
 class GpsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gps)
+
+        gps_bt_localizar.setOnClickListener {
+            val uri = Uri.parse("geo:0,0?q=${gps_et_search.text}")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
