12a2386ee18cbea12eea1aa0d755b21b6de7887f
Sergio <sergiorp6@gmail.com>
2019-03-11 20:28:10 +0100

    Open web browser when clicking button using implicit intent

--- IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/WebActivity.kt
+++ IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/WebActivity.kt
@@ -1,13 +1,21 @@
 package es.pue.intentspractice.presentationlayer.controllers.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import es.pue.intentspractice.R
+import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+
+        web_bt_navegar.setOnClickListener {
+            val uri = Uri.parse(web_et_web.text.toString())
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
