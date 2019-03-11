b21ec3632d37dda18cfa17c287f4bc6da72698a2
Sergio <sergiorp6@gmail.com>
2019-03-11 21:08:54 +0100

    Manage ActivityNotFoundException

--- IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/GpsActivity.kt
+++ IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/GpsActivity.kt
@@ -1,9 +1,11 @@
 package es.pue.intentspractice.presentationlayer.controllers.activities
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import es.pue.intentspractice.R
 import kotlinx.android.synthetic.main.activity_gps.*
 
@@ -16,7 +18,12 @@ class GpsActivity : AppCompatActivity() {
         gps_bt_localizar.setOnClickListener {
             val uri = Uri.parse("geo:0,0?q=${gps_et_search.text}")
             val intent = Intent(Intent.ACTION_VIEW, uri)
-            startActivity(intent)
+
+            try {
+                startActivity(intent)
+            } catch (ex: ActivityNotFoundException) {
+                Toast.makeText(this, "No se ha encontrado ninguna app.", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
