59c338f03a6ab9ca04b5d7cf70a6feba88cd4a25
Sergio <sergiorp6@gmail.com>
2019-03-11 19:51:08 +0100

    Start a second activity when a button is clicked

--- IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/MainActivity.kt
+++ IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/MainActivity.kt
@@ -1,12 +1,26 @@
-package es.pue.intentspractice
+package es.pue.intentspractice.presentationlayer.controllers.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import es.pue.intentspractice.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_btWeb.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        var intent: Intent? = null
+
+        when (v?.id) {
+            R.id.main_btWeb -> intent = Intent(this, WebActivity::class.java)
+        }
+        startActivity(intent)
     }
 }
