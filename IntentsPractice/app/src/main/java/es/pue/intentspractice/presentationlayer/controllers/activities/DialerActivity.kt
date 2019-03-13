f27ce3d6fa6eee80757f7d94657587b6cb7eabbc
Sergio <sergiorp6@gmail.com>
2019-03-13 20:07:11 +0100

    Use phone application requesting call permission

--- IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/DialerActivity.kt
+++ IntentsPractice/app/src/main/java/es/pue/intentspractice/presentationlayer/controllers/activities/DialerActivity.kt
@@ -1,8 +1,14 @@
 package es.pue.intentspractice.presentationlayer.controllers.activities
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.widget.Button
 import android.widget.TableRow
 import es.pue.intentspractice.R
@@ -20,7 +26,22 @@ class DialerActivity : AppCompatActivity() {
                 val view = fila.getChildAt(j)
                 if (view is Button) {
                     view.setOnClickListener {
-                        dialer_etPhoneNUmber.setText(dialer_etPhoneNUmber.text.toString() + view.text.toString())
+                        dialer_etPhoneNUmber.setText("${dialer_etPhoneNUmber.text}${view.text}")
+                    }
+                }
+                if (view === dialer_btCall) {
+                    view.setOnClickListener {
+                        val uri = Uri.parse("tel:${dialer_etPhoneNUmber.text}")
+                        val intent = Intent(Intent.ACTION_CALL, uri)
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.CALL_PHONE
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+                        }
+                        startActivity(intent)
                     }
                 }
             }
