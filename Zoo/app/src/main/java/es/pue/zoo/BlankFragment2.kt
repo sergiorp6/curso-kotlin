35379e03723a2584055280748973b7214d482b82
unknown <kotlin@aula.local>
2019-04-29 21:28:07 +0200

    Show fragment with a list of names using ViewHolder pattern

--- Zoo/app/src/main/java/es/pue/zoo/BlankFragment2.kt
+++ Zoo/app/src/main/java/es/pue/zoo/BlankFragment2.kt
@@ -1,30 +1,28 @@
 package es.pue.zoo
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class BlankFragment2 : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_fragment2, container, false)
+        val view = inflater.inflate(R.layout.fragment_blank_fragment2, container, false)
+        val nombres = context?.resources?.getStringArray(R.array.nombres)?.toList()
+        val layout = LinearLayoutManager(activity)
+        val items = view.findViewById<RecyclerView>(R.id.recyclerView_items)
+
+        items.layoutManager = layout
+        items.adapter = ItemsAdapter(nombres!!)
+
+        return view
     }
 
     companion object {
